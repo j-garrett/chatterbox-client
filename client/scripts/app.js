5b70577edf9b176a6fd520b49bea80f6b919ce99
Jon Garrett <jonathan.garrett@gmail.com>
2016-08-08 12:27:22 -0700

    Complete init, sending, fetching, and some chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,45 @@
 // YOUR CODE HERE:
 
+
+var app = {};
+app.init = function() {};
+app.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox; Failed to send message', data);
+    }
+  });
+};
+app.fetch = function() {
+  $.ajax({
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function(data) {
+      console.error('chatterbox; Failed to receive message', data);
+    }
+  });
+};
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+app.addMessage = function(message) {
+  //create container for message
+  var messageBox = $('<div class="message-box"></div>');
+  //create components for message parts
+  var user = $(`<div class="username">${message.username}:</div>`);
+  var message = $(`<div class="message-text">${message.text}</div>`);
+
+  messageBox.append(user, message);
+  $('#chats').append(messageBox);
+};
